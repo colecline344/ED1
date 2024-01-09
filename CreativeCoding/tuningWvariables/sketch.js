582ff37acf80d476775116c29a5994b9847eb59d
colecline344 <cdrive292@gmail.com>
2024-01-09 11:49:44 -0500

    update

--- CreativeCoding/tuningWvariables/sketch.js
+++ CreativeCoding/tuningWvariables/sketch.js
@@ -1,7 +1,34 @@
 function setup() {//runs one time
+createCanvas (windowWidth, windowHeight);
+  fill('yellow');
+  strokeWeight (1);
 
 }
 
 function draw() {//runs in a loop
+  
+  var num = 10
 
+  var sideLen = windowWidth/num;
+
+  
+  for (var y = 0; y < windowHeight; y = y + sideLen) 
+{
+  
+  for (var x = 0; x < windowWidth; x = x + sideLen) 
+{
+    quad (
+    x,y,
+    x + 360,y,
+    x + 360,y + 360,
+    x,y + 360  
+  );//loop to create a row in the x direction
+  
+  }
+}
+  
+}
+
+function windowResized(){
+  resizeCanvas(windowWidth, windowHeight)
 }
